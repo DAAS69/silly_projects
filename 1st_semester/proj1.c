03f2b7a7da0dd6111b5453278496cb1810cde938
Saad Tauseef <126574907+DAAS69@users.noreply.github.com>
2024-09-30 19:18:06 +0500

    proj1.c

---
+++ 1st_semester/proj1.c
@@ -0,0 +1,51 @@
+/*
+The following code take digits as an input and coverts the digit in alphabetic in reverse order.
+Example:
+Input: 12
+output: two one
+Input: 1234
+Output: four three two one
+*/
+
+#include <stdio.h>
+int main() {
+int num, checker;
+printf("please enter any number:");
+scanf("%d", &num);
+while (num>0){
+checker = (num % 10);
+switch (checker){
+case 0:
+printf("zero ");
+break;
+case 1:
+printf("one ");
+break;
+case 2:
+printf("two ");
+break;
+case 3:
+printf("three ");
+break;
+case 4:
+printf("four ");
+break;
+case 5:
+printf("five ");
+break;
+case 6:
+printf("six ");
+break;
+case 7:
+printf("seven ");
+break;
+case 8:
+printf("eight ");
+break;
+case 9:
+printf("nine ");
+break;
+}
+num = num/10;
+}
+}
